362ec290d0dbe2664907c5fd381b29fdd9c2a457
Abhilash Jha <abhilashjha121@gmail.com>
2026-04-15 00:16:30 +0530

    submited

--- src/components/App.js
+++ src/components/App.js
@@ -7,20 +7,19 @@ import FormState from "./FormState";
 
 export default function App() {
   return (
-    
-<>
+    <>
       <nav>
-       <NavLink to="/form-link">Form</NavLink>
-<NavLink to="/form-ref-link">FormRef</NavLink>
-<NavLink to="/form-state-link">FormState</NavLink>
+        <NavLink to="/form-link">Form</NavLink> |{" "}
+        <NavLink to="/form-ref-link">FormRef</NavLink> |{" "}
+        <NavLink to="/form-state-link">FormState</NavLink>
       </nav>
 
       <Switch>
-        <Route path="/form-link" component={Form} />
-        <Route path="/form-ref-link" component={FormRef} />
-        <Route path="/form-state-link" component={FormState} />
+        <Route exact path="/" component={Form} />
+        <Route exact path="/form-link" component={Form} />
+        <Route exact path="/form-ref-link" component={FormRef} />
+        <Route exact path="/form-state-link" component={FormState} />
       </Switch>
-
     </>
   )
 }
